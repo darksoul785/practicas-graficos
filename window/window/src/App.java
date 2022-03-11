aba5477080f998136c4d429451a3d6a5e027becb
David <a19310211@ceti.mx>
2022-03-10 19:16:41 -0600

    se agrega la practica de ventana

--- window/window/src/App.java
+++ window/window/src/App.java
@@ -1,7 +1,12 @@
-import java.awt.*;
+import javax.swing.JFrame;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        JFrame P = new JFrame();
+        P.setSize(300, 300);
+        P.setTitle("ejemplito");
+        P.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        P.setLocationRelativeTo(null);
+        P.setVisible(true);;
     }
 }
