919bb4f15fcde5fcdc7d271379cb47b496a32934
David <a19310211@ceti.mx>
2022-03-14 00:55:37 -0600

    ...cambios a scroll

--- visor2/src/Visor.java
+++ visor2/src/Visor.java
@@ -1,16 +1,9 @@
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Label;
-import java.io.File;
-
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JRootPane;
 import javax.swing.JScrollPane;
 import javax.swing.plaf.DimensionUIResource;
-import javax.swing.plaf.ScrollBarUI;
 
 public class Visor extends JFrame {
 
@@ -23,14 +16,17 @@ public class Visor extends JFrame {
         scroll.setViewportView(img);
 
         panel.setLayout(null);
-        panel.setPreferredSize(new DimensionUIResource(410, 410));
+        // panel.setPreferredSize(new DimensionUIResource(410, 410));
         panel.setVisible(true);
         scroll.getVerticalScrollBar().setUnitIncrement(16);
         add(scroll);
     }
+
     public static void main(String[] args) {
         Visor visor = new Visor();
-        visor.setBounds(50, 50, 400, 400);
+        visor.setSize(500, 500);
+        visor.setLocationRelativeTo(null);
+        // visor.setBounds(50, 50, 400, 400);
         visor.setVisible(true);
         visor.setResizable(true);
     }
